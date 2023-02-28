5f130a2e1e4c661bcebfcf5de56d6a98eb9e7cc5
Sarah Dalrymple <47044679+SarahEDalrymple@users.noreply.github.com>
2023-02-28 16:04:31 +0000

    Added draft R script for Red Listing workshop

---
+++ IUCNRedListing.R
@@ -0,0 +1,25 @@
+
+################################
+#                              #
+#      IUCN Red Listing        #
+#                              #
+################################
+
+# Birkenhead College @ LJMU
+# 8 March 2023
+
+# go to 
+
+
+# Forest coconut assessment
+#--------------------------
+
+data <- read.table()
+year_seen <- c(1989, 1986, 1986,2005,1986,1986,1986,1986,1986,
+               2008,1989,1986,1986,1989,1986,2015,1986,
+               1989,1986,1986,1986,2015,1987,2015,1990,2015)
+
+# GBIF data: GBIF.org (28 February 2023) GBIF Occurrence Download https://doi.org/10.15468/dl.8xrmxy
+
+hist(year_seen)
+
