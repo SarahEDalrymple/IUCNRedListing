1a5387698bd33527389d243cd37fb65831e8241f
Sarah Dalrymple <47044679+SarahEDalrymple@users.noreply.github.com>
2023-03-08 09:34:45 +0000

    Completed script

--- IUCNRedListing.R
+++ IUCNRedListing.R
@@ -8,7 +8,8 @@
 # Birkenhead College @ LJMU
 # 8 March 2023
 
-# go to 
+# script available at:
+# https://github.com/SarahEDalrymple/IUCNRedListing
 
 
 # Forest coconut assessment #
@@ -35,3 +36,37 @@ hist(year_seen, xlab = "", ylab = "", main = "", col = "")
 hist(year_seen, xlab = "Year", ylab = , main = "Forest coconut records by year", 
      col = "gold", breaks = (max(year_seen)-(min(year_seen))))
 
+
+# Delacour's langur assessment #
+################################
+
+# GBIF data: 
+
+year_seen <- c(2009,2023,2019,2023,2006,2018,
+               2020,2020,2018,2020,2017,2014,
+               2020,2019,2019,2019,1930,2018,
+               2008,2016,2018,2004)
+
+# plot the data in a histogram over time
+
+hist(year_seen, xlab = "Year", ylab = , main = "Delacour's langur records by year", 
+     col = "lightblue", breaks = (max(year_seen)-(min(year_seen))))
+
+# building a dataset
+
+surveys <- c(2000, 2010, 2015)
+total_population <- c(302, 317, 275)
+number_of_groups <- c(53, 57, NA)
+number_of_subpopulations <- c(19, 14, 8)
+
+langur_population <- cbind(surveys, total_population)
+langur_population
+
+# try creating a table of all the data by adapting the code above
+
+langur_data <- cbind()
+
+# try plotting the data in different combinations using the code below
+
+plot(surveys, total_population)
+     
